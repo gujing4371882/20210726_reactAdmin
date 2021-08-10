0f3b5e42d3a1575fc32372385555338902ef1c90
gujing4371882 <15209867164@139.com>
2021-08-10 18:10:55 +0800

    补充

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -5,11 +5,10 @@
  * */
 import axios from  'axios'
 import { Message } from 'antd'
-
 import qs from 'qs'
 
 // 请求拦截器
-axios.interceptors.request.use( config => {
+axios.interceptors.request.use( config => {  
   // 请求方式和请求体数据格式
   const { method, data } = config
   if(method.toLowerCase() === 'post' && typeof data === 'object') {
@@ -21,8 +20,12 @@ axios.interceptors.request.use( config => {
 
 // 响应拦截器
 axios.interceptors.response.use (response => {
-  // 获取成功的结果 data对象
-  const { data } = response.data
+  // 获取成功的结果 data对象   
+  const { data, status, msg } = response.data
+  // 业务逻辑统一判断
+  if(status !== 0) {
+    Message.warning(msg)
+  }
   return data
 }, err => {
   // return Promise.reject(err)
